f13301bde6555d977a2b8b6f1d14f838dbafcf23
Natnicha <nat.rodtong@gmail.com>
2024-06-05 18:57:04 +0200

    feat: handle exception

--- app/api/ml/ml.py
+++ app/api/ml/ml.py
@@ -21,21 +21,26 @@ async def classify(
             status_code=status.HTTP_400_BAD_REQUEST
         )
     
-    # Step 1: Initialize model with the best available weights
-    weights = EfficientNet_B3_Weights.DEFAULT
-    model = efficientnet_b3(weights=weights)
-    model.eval()
+    try:
+        # Step 1: Initialize model with the best available weights
+        weights = EfficientNet_B3_Weights.DEFAULT
+        model = efficientnet_b3(weights=weights)
+        model.eval()
 
-    # Step 2: Initialize the inference transforms
-    preprocess = weights.transforms()
+        # Step 2: Initialize the inference transforms
+        preprocess = weights.transforms()
 
-    # Step 3: Apply inference preprocessing transforms
-    batch = preprocess(img).unsqueeze(0)
+        # Step 3: Apply inference preprocessing transforms
+        batch = preprocess(img).unsqueeze(0)
 
-    # Step 4: Use the model and print the predicted category
-    prediction = model(batch).squeeze(0).softmax(0)
-    class_id = prediction.argmax().item()
-    score = prediction[class_id].item()
-    category_name = weights.meta["categories"][class_id]
-
+        # Step 4: Use the model and print the predicted category
+        prediction = model(batch).squeeze(0).softmax(0)
+        class_id = prediction.argmax().item()
+        score = prediction[class_id].item()
+        category_name = weights.meta["categories"][class_id]
+    except Exception as e:
+        raise HTTPException(
+            detail={"message": str(e)},
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
     return ClassifyResponseModel(name=category_name, score=f"{100*score:.1f}")
